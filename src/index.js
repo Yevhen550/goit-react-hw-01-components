1e7e70807e8dd8c8eb58478fda9ab90ede571830
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-03-13 20:41:50 +0200

    01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const parahraph = React.createElement('p', {
+//   id: 'test-ID',
+//   children: ['I am Lion'],
+// });
+
+// const p = <p id="test- id">I am LION</p>;
+
+// const div = (
+//   <div name="main" id="test-div-id" className="test-class">
+//     {p}
+//   </div>
+// );
+
+// ReactDOM.createRoot(document.getElementById('root')).render(div);
